f137d5ca65324242f8a2ad38fd6f8e221426d6cc
Kolya Venturi <kolya.venturi@enx3s.com>
2020-09-19 16:24:26 -0700

    Render animation

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
+import lottie, {AnimationItem} from 'lottie-web';
+
+import animationData from './icon.json';
 
 export type IconProps = {
   readonly size: number;
 };
 
-const Icon = ({size = 96}: IconProps): JSX.Element => (
-  <div>
-    <p>text {size}</p>
-  </div>
-);
+const Icon = ({size = 48}: IconProps): JSX.Element => {
+  const ref = useRef<null | HTMLDivElement>(null);
+  const [anim, setAnim] = useState<null | AnimationItem>(null);
+
+  console.log(size);
+  useEffect(() => {
+    if (!ref?.current) {
+      return;
+    }
+
+    const ani = lottie.loadAnimation({
+      container: ref.current,
+      renderer: 'svg',
+      loop: false,
+      autoplay: false,
+      animationData
+    });
+
+    ani.addEventListener('complete', () => {
+      ani.goToAndStop(0, true);
+    });
+
+    setAnim(ani);
+  }, [ref]);
+
+  const handleMouseOver = () => {
+    if (anim) {
+      anim.play();
+    }
+  };
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        cursor: 'pointer',
+        width: size,
+        height: size
+      }}
+      onMouseOver={handleMouseOver}
+    />
+  );
+};
 
 export default Icon;
